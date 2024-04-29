31417cdb87fc55e60865bc6ccd9b8f7218031f34
ankitsubhamjyoti2005 <ankitsubhamjyoti@gmail.com>
2024-04-29 23:14:03 +0530

    new file:   jenkin-server/data.tf 	modified:   jenkin-server/main.tf

---
+++ jenkin-server/data.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "example" {
+
+  most_recent      = true
+
+  owners           = ["amazon"]
+
+  filter {
+    name   = "ubuntu"
+    values = ["myami-*ami-04b70fa74e45c3917"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
